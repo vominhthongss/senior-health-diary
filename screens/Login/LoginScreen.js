0e6e391e5d51c8f7504090c5ebc66b4fb139fa2e
vominhthongss <vominhthongss@gmail.com>
2023-12-09 12:34:36 +0900

    feat: create HomeScreen.js and LoginScreen.js

---
+++ screens/Login/LoginScreen.js
@@ -0,0 +1,51 @@
+// LoginScreen.js
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+
+const LoginScreen = ({ navigation }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Thực hiện xác thực và đăng nhập logic ở đây
+    // Nếu thành công, chuyển đến màn hình chính
+    navigation.navigate('Home');
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text>Login Screen</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        value={username}
+        onChangeText={setUsername}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <Button title="Login" onPress={handleLogin} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    width: '80%',
+    marginVertical: 10,
+    padding: 10,
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+});
+
+export default LoginScreen;
