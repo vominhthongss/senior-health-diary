7366fb0aa0cf6acb62593cdeb6453876a69f55eb
vominhthongss <vominhthongss@gmail.com>
2023-12-10 15:42:56 +0900

    create 1 file and update 1 file

---
+++ src/components/CustomizeTextInput/CustomizetextInput.js
@@ -0,0 +1,22 @@
+import { TextInput, View } from "react-native";
+
+function CustomizeTextInput({
+  placeholder,
+  value,
+  onChangeText,
+  secureTextEntry,
+}) {
+  return (
+    <View className="w-full">
+      <TextInput
+        className="p-5 border rounded-md"
+        placeholder={placeholder}
+        value={value}
+        onChangeText={onChangeText}
+        secureTextEntry={secureTextEntry}
+      />
+    </View>
+  );
+}
+
+export default CustomizeTextInput;
--- src/screens/Login/LoginScreen.js
+++ src/screens/Login/LoginScreen.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { View, Text, TextInput, Button } from "react-native";
 import * as SCREENS_NAME from "../../constants/screensName";
 import CustomizeButton from "../../components/CustomizeButton/CustomizeButton";
+import CustomizeTextInput from "../../components/CustomizeTextInput/CustomizetextInput";
 const LoginScreen = ({ navigation }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -13,19 +14,22 @@ const LoginScreen = ({ navigation }) => {
   return (
     <View className="h-full w-full flex justify-center items-center space-y-2">
       <Text>Login Screen</Text>
-      <TextInput
-        className="p-5 border rounded-md w-[80%]"
-        placeholder="Username"
-        value={username}
-        onChangeText={setUsername}
-      />
-      <TextInput
-        className="p-5 border rounded-md w-[80%]"
-        placeholder="Password"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-      />
+      <View className="w-[80%]">
+        <CustomizeTextInput
+          placeholder={"Username"}
+          value={username}
+          onChangeText={setUsername}
+          secureTextEntry={false}
+        />
+      </View>
+      <View className="w-[80%]">
+        <CustomizeTextInput
+          placeholder={"Password"}
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry={true}
+        />
+      </View>
       <View className="w-[80%]">
         <CustomizeButton title={"Login"} onPress={handleLogin} />
       </View>
