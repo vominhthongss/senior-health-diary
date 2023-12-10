0a0144150f7bf1e3b1af84d95a1c4b57fe190a97
vominhthongss <vominhthongss@gmail.com>
2023-12-10 14:22:47 +0900

    update LoginScreen.js and tailwind.config.js

--- src/screens/Login/LoginScreen.js
+++ src/screens/Login/LoginScreen.js
@@ -10,39 +10,41 @@ const LoginScreen = ({ navigation }) => {
   };
 
   return (
-    <View style={styles.container}>
+    <View className="h-full w-full flex justify-center items-center space-y-2">
       <Text>Login Screen</Text>
       <TextInput
-        style={styles.input}
+        className="p-5 border rounded-md w-[80%]"
         placeholder="Username"
         value={username}
         onChangeText={setUsername}
       />
       <TextInput
-        style={styles.input}
+        className="p-5 border rounded-md w-[80%]"
         placeholder="Password"
         value={password}
         onChangeText={setPassword}
         secureTextEntry
       />
-      <Button title="Login" onPress={handleLogin} />
+      <View className="bg-main w-[80%] rounded-md">
+        <Button color={"white"} title="Login" onPress={handleLogin} />
+      </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  input: {
-    width: "80%",
-    marginVertical: 10,
-    padding: 10,
-    borderWidth: 1,
-    borderRadius: 5,
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: "center",
+//     alignItems: "center",
+//   },
+//   input: {
+//     width: "80%",
+//     marginVertical: 10,
+//     padding: 10,
+//     borderWidth: 1,
+//     borderRadius: 5,
+//   },
+// });
 
 export default LoginScreen;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [],
-  content: [
-    "./App.{js,jsx,ts,tsx}",
-    "./src/screens/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./App.{js,jsx,ts,tsx}", "./src/screens/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: "#1876d1",
+      },
+    },
   },
   plugins: [],
 };
