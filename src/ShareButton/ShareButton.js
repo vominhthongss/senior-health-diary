fd75d2af7a617d942b7b8e2c677b443b8395fb33
vominhthongss <vominhthongss@gmail.com>
2023-12-14 21:10:32 +0900

    update 4 files and create 1 file

---
+++ src/ShareButton/ShareButton.js
@@ -0,0 +1,35 @@
+import { TouchableHighlight, View } from "react-native";
+import * as COLORS from "../constants/colors";
+import * as FileSystem from "expo-file-system";
+import * as Sharing from "expo-sharing";
+import * as STRINGS from "../constants/strings";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { TouchableOpacity } from "react-native-gesture-handler";
+function ShareButton({ symptoms, description, date }) {
+  const shareHealthLog = async () => {
+    try {
+      const pdfContent = `Triệu chứng: ${symptoms}\nMô tả chi tiết:${description}\nNgày: ${date}`;
+      const path = `${FileSystem.documentDirectory}HealthyLog.pdf`;
+      await FileSystem.writeAsStringAsync(path, pdfContent, {
+        encoding: FileSystem.EncodingType.UTF8,
+      });
+
+      await Sharing.shareAsync(path, {
+        mimeType: "application/pdf",
+        dialogTitle: STRINGS.share_health_log,
+      });
+    } catch (error) {
+      console.error("Error sharing health log:", error.message);
+    }
+  };
+
+  return (
+    <View>
+      <TouchableOpacity onPress={shareHealthLog}>
+        <Icon name="share" size={"20"} color={COLORS.main} />
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+export default ShareButton;
--- src/screens/Schedule/ScheduleScreen.js
+++ src/screens/Schedule/ScheduleScreen.js
@@ -6,18 +6,28 @@ import * as STRINGS from "../../constants/strings";
 import { localeConfig } from "../../constants/localeConfig";
 import { ScrollView } from "react-native-gesture-handler";
 import CustomizeButton from "../../components/CustomizeButton/CustomizeButton";
+import ShareButton from "../../ShareButton/ShareButton";
 
 LocaleConfig.locales["vi"] = localeConfig;
 LocaleConfig.defaultLocale = "vi";
 
 function ScheduleScreen() {
   const [items, setItems] = useState({
-    "2023-12-01": [{ time: "12:00", text: "Meeting at 10am" }],
+    "2023-12-01": [{ type: "remind", time: "12:00", text: "Meeting at 10am" }],
     "2023-12-12": [
-      { time: "12:00", text: "Lunch with colleagues" },
-      { time: "12:00", text: "Shopping" },
+      { type: "remind", time: "12:00", text: "Lunch with colleagues" },
+      { type: "remind", time: "12:00", text: "Shopping" },
     ],
-    "2023-12-13": [{ time: "12:00", text: "Gym at 5pm" }],
+    "2023-12-13": [{ type: "remind", time: "12:00", text: "Gym at 5pm" }],
+    "2023-12-14": [
+      {
+        type: "diary",
+        sick: "Cao huyết áp",
+        symptoms: "Nhức đầu",
+        description: "Đau hoài",
+        date: "2023-12-14",
+      },
+    ],
   });
 
   const [newDate, setNewDate] = useState("");
@@ -26,10 +36,29 @@ function ScheduleScreen() {
   const renderItem = (item) => {
     return (
       <ScrollView>
-        <View className="flex flex-column items-start justify-center shadow-md bg-gray-300 my-2 mr-2 rounded-md">
-          <Text className="text-sm">{item.time}</Text>
-          <Text className="text-sm">{item.text}</Text>
-        </View>
+        {item.type === "remind" ? (
+          <View className="flex flex-column items-start justify-center shadow-md bg-gray-300 my-2 mr-2 rounded-md">
+            <Text className="text-sm font-bold text-green-700">
+              {item.time}
+            </Text>
+            <Text className="text-sm">{item.text}</Text>
+          </View>
+        ) : (
+          <View className="relative w-full flex flex-column items-start justify-center shadow-md bg-gray-300 my-2 mr-2 rounded-md">
+            <Text className="text-lg font-bold">{item.sick}</Text>
+            <Text className="text-md">{item.symptoms}</Text>
+            <Text className="text-sm italic text-gray-500">
+              {item.description}
+            </Text>
+            <View className="absolute right-1 bottom-0">
+              <ShareButton
+                symptoms={item.symptoms}
+                description={item.description}
+                date={item.date}
+              />
+            </View>
+          </View>
+        )}
       </ScrollView>
     );
   };
