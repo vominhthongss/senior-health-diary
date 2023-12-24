db1bc3ed0af0e38c49b56767991fc6f2f0d74b98
vominhthongss <vominhthongss@gmail.com>
2023-12-24 12:57:50 +0900

    update screensName.js

--- src/constants/screensName.js
+++ src/constants/screensName.js
@@ -6,5 +6,5 @@ export const home = "Home";
 export const account = "Account";
 export const schedule = "Schedule";
 export const diary = "Diary";
-export const changePassword = "changePassword";
+export const changePassword = "ChangePassword";
 export const sickDetail = "SickDetail";
