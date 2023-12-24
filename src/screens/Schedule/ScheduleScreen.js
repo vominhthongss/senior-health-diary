2bd5bce765dedddba36f744d4c02106bcf731376
Tan Tran <tantranstst@gmail.com>
2023-12-24 12:30:43 +0700

    update submit schedule

--- src/screens/Schedule/ScheduleScreen.js
+++ src/screens/Schedule/ScheduleScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, Alert } from "react-native";
+import { View, Text, Alert, Modal, TouchableOpacity } from "react-native";
 import { Agenda, LocaleConfig } from "react-native-calendars";
 import * as COLORS from "../../constants/colors";
 import * as STRINGS from "../../constants/strings";
@@ -9,11 +9,8 @@ import CustomizeButton from "../../components/CustomizeButton/CustomizeButton";
 import ShareButton from "../../ShareButton/ShareButton";
 import { useDispatch, useSelector } from "react-redux";
 import _ from "lodash";
-import {
-  fetchSchedule,
-  updateSchedule,
-} from "../../store/schedule/scheduleSlice";
-import RemindOrDiaryModal from "../Modals/RemindOrDiaryModal";
+import { fetchSchedule } from "../../store/schedule/scheduleSlice";
+import GeneralForm from "../../components/GeneralForm/GeneralForm";
 
 LocaleConfig.locales["vi"] = localeConfig;
 LocaleConfig.defaultLocale = "vi";
@@ -64,18 +61,104 @@ function ScheduleScreen() {
   };
   const [dataSelected, setDateSelected] = useState("");
   const [remindText, setRemindText] = useState("text");
+  const [diaryText, setDiaryText] = useState("text");
 
-  const [isModalVisible, setModalVisible] = useState(false);
-  const [inputData, setInputData] = useState('');
+  const [modalRemindVisible, setModalRemindVisible] = useState(false);
+  const [modalDiaryVisible, setModalDiaryVisible] = useState(false);
+  const [inputData, setInputData] = useState("");
 
+  const fields = [
+    {
+      name: "time",
+      placeholder: "Thời gian",
+      value: "",
+      type: "text",
+      label: "Thời gian",
+      isRequired: true,
+    },
+    {
+      name: "content",
+      placeholder: "Nội dung",
+      value: "",
+      type: "text",
+      label: "Nội dung",
+      isRequired: true,
+    },
+  ];
+
+  const fieldDiary = [
+    {
+      name: "sick",
+      placeholder: "Tên bệnh",
+      value: "",
+      type: "text",
+      label: "Tên bệnh",
+      isRequired: true,
+      status: 0,
+    },
+    {
+      name: "symptoms",
+      placeholder: "Triệu chứng",
+      value: "",
+      type: "text",
+      label: "Triệu chứng",
+      isRequired: true,
+      status: 0,
+    },
+    {
+      name: "description",
+      placeholder: "Mô tả",
+      value: "",
+      type: "text",
+      label: "Mô tả",
+      isRequired: true,
+      status: 0,
+    },
+    {
+      name: "date",
+      placeholder: "Ngày",
+      value: "",
+      type: "text",
+      label: "Ngày",
+      isRequired: true,
+    },
+  ];
   const handleSaveData = (data) => {
     // Xử lý logic khi nhận dữ liệu từ modal
     setInputData(data);
   };
 
-  const handleAddDate = () => {
-    setModalVisible(true)
-    if (!dataSelected || !remindText) {    
+  const handleAddRemind = () => {
+    setModalRemindVisible(true);
+    if (!dataSelected || !remindText) {
+      setModalRemindVisible(true);
+      return;
+    }
+    // trường hợp là "remind"
+    // dispatch(
+    //   updateSchedule({
+    //     date: dataSelected,
+    //     schedule: { type: "remind", time: "12:00", text: remindText },
+    //   })
+    // );
+    // trường hợp là "diary"›
+    // dispatch(
+    //   updateSchedule({
+    //     date: dataSelected,
+    //     schedule: {
+    //       type: "diary",
+    //       sick: "Mỏi",
+    //       symptoms: "Đau chân",
+    //       description: "Mỏi",
+    //       date: dataSelected,
+    //     },
+    //   })
+    // );
+  };
+  const handleAddDiary = () => {
+    setModalDiaryVisible(true);
+    if (!dataSelected || !diaryText) {
+      setModalDiaryVisible(true);
       return;
     }
     // trường hợp là "remind"
@@ -104,6 +187,10 @@ function ScheduleScreen() {
     100
   );
 
+  const handleFormSubmit = (values) => {
+    console.log("Form Data in Parent Component:", values);
+  };
+
   return (
     <View className="flex-1">
       <Agenda
@@ -119,13 +206,78 @@ function ScheduleScreen() {
         renderEmptyData={renderEmptyData}
       />
 
-      <View className="absolute bottom-1 right-0 w-[100%] px-2">
-        <CustomizeButton onPress={handleAddDate} title={STRINGS.addDate} />
-        <RemindOrDiaryModal
-        isVisible={isModalVisible}
-        onClose={() => setModalVisible(false)}
-        onSave={handleSaveData}
-      />
+      <View className="absolute bottom-1 right-0 w-[100%] px-2 ">
+        <Modal
+          animationType="fade"
+          transparent={true}
+          visible={modalRemindVisible}
+          onRequestClose={() => {
+            Alert.alert("Modal has been closed.");
+            // Handle any additional actions when the modal is closed
+          }}
+        >
+          <View className="flex-1 justify-center content-center items-center">
+            <View className="bg-white p-4 rounded-lg w-80">
+              <TouchableOpacity
+                className="absolute top-2 right-2"
+                onPress={() => setModalRemindVisible(!modalRemindVisible)}
+              >
+                <Text>X</Text>
+              </TouchableOpacity>
+              <Text className="text-center text-lg font-bold mb-4">
+                Thêm nhắc nhở
+              </Text>
+              <GeneralForm
+                fields={fields}
+                titleSubmitBtn={STRINGS.addRemind}
+                handleData={handleFormSubmit}
+              />
+            </View>
+          </View>
+        </Modal>
+      </View>
+      <View className="absolute bottom-1 right-0 w-[100%] px-2 ">
+        <Modal
+          animationType="fade"
+          transparent={true}
+          visible={modalDiaryVisible}
+          onRequestClose={() => {
+            Alert.alert("Modal has been closed.");
+            setModalDiaryVisible(!modalDiaryVisible);
+          }}
+        >
+          <View className="flex-1 justify-center content-center items-center">
+            <View className="bg-white p-4 rounded-lg w-80">
+              <TouchableOpacity
+                style={{
+                  position: "absolute",
+                  top: 8,
+                  right: 8,
+                }}
+                onPress={() => setModalDiaryVisible(!modalDiaryVisible)}
+              >
+                <Text>X</Text>
+              </TouchableOpacity>
+              <Text className="text-center text-lg font-bold mb-4">
+                Thêm nhật ký
+              </Text>
+              <GeneralForm
+                fields={fieldDiary.map((field) =>
+                  field.name === "date"
+                    ? { ...field, value: dataSelected }
+                    : field
+                )}
+                titleSubmitBtn={STRINGS.addDiary}
+                handleData={handleFormSubmit}
+              ></GeneralForm>
+            </View>
+          </View>
+        </Modal>
+      </View>
+
+      <View className="flex justify-between flex-row w-full">
+        <CustomizeButton onPress={handleAddRemind} title={STRINGS.addRemind} />
+        <CustomizeButton onPress={handleAddDiary} title={STRINGS.addDiary} />
       </View>
     </View>
   );
