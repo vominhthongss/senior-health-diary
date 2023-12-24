1b4d7504884eaca631f993c5311d662a9d1b266e
vominhthongss <vominhthongss@gmail.com>
2023-12-24 13:06:06 +0900

    update db.json and SickDetail.js

--- src/screens/SickDetail/SickDetail.js
+++ src/screens/SickDetail/SickDetail.js
@@ -1,6 +1,16 @@
+import { useEffect } from "react";
 import { View } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUser } from "../../store/userInformation/userInformationSlice";
 
 function SickDetailScreen() {
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.userInformation);
+  useEffect(() => {
+    if (!user) {
+      dispatch(fetchUser());
+    }
+  }, [user, dispatch]);
   return <View></View>;
 }
 
