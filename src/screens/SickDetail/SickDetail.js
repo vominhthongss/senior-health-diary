cc09e8990978767ce17cb8f24c3e9c52f1ddb2f2
vominhthongss <vominhthongss@gmail.com>
2023-12-24 13:36:06 +0900

    update strings.js and SickDetail.js

--- src/screens/SickDetail/SickDetail.js
+++ src/screens/SickDetail/SickDetail.js
@@ -4,6 +4,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchUser } from "../../store/userInformation/userInformationSlice";
 import { fetchSavedSicks } from "../../store/sickDetail/sickDetailSlice";
 import CustomizeButton from "../../components/CustomizeButton/CustomizeButton";
+import * as STRINGS from "../../constants/strings";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 function SickDetailScreen() {
   const dispatch = useDispatch();
@@ -22,14 +24,40 @@ function SickDetailScreen() {
     }
   }, [user, savedSicks, dispatch]);
   return (
-    <View className="flex flex-row justify-center">
+    <View className="flex flex-row justify-center relative">
       <View className="w-[90%]">
-        <Text>{isSaved && "save"}</Text>
-        <ScrollView className="h-full">
-          <Text>{sick.name}</Text>
+        <View className="flex flex-row items-center space-x-2 mt-2">
+          <Icon size={30} name="heart" color={"red"} />
+          <Text className="text-xl">{isSaved && STRINGS.saveSick}</Text>
+        </View>
+        <ScrollView className="h-full mt-5">
+          <View>
+            <Text className="text-xl font-bold uppercase">
+              {STRINGS.sickName}
+            </Text>
+            <Text className="text-xl">{sick?.name}</Text>
+          </View>
+          <View>
+            <Text className="text-xl font-bold uppercase">
+              {STRINGS.sickReason}
+            </Text>
+            <Text className="text-xl">{sick?.name}</Text>
+          </View>
+          <View>
+            <Text className="text-xl font-bold uppercase">
+              {STRINGS.sickSimptom}
+            </Text>
+            <Text className="text-xl">{sick?.name}</Text>
+          </View>
+          <View>
+            <Text className="text-xl font-bold uppercase">
+              {STRINGS.sickRevention}
+            </Text>
+            <Text className="text-xl">{sick?.name}</Text>
+          </View>
         </ScrollView>
-        <View className="absolute bottom-14 w-full">
-          <CustomizeButton title={"Lưu thông tin bệnh này"} />
+        <View className="absolute bottom-20 w-full">
+          <CustomizeButton title={STRINGS.savesickBtn} />
         </View>
       </View>
     </View>
