3464c880e7a9dc57c24b79164686a7dd8630d091
vominhthongss <vominhthongss@gmail.com>
2024-02-03 17:13:05 +0900

    update app.json and Notification.js

--- src/components/Notification/Notification.js
+++ src/components/Notification/Notification.js
@@ -2,8 +2,12 @@ import { useEffect } from "react";
 import * as Notifications from "expo-notifications";
 
 export function useSetReminders({ value }) {
-  const scheduleNoti = () => {
+  const scheduleNoti = async () => {
     if (!value) return; // Check if value is defined
+
+    // Lấy Expo Push Token
+    const expoPushToken = await registerForPushNotificationsAsync();
+    console.log("Expo Push Token:", expoPushToken);
 
     Object.keys(value).forEach((key) => {
       value[key].forEach(async (schedule) => {
@@ -16,20 +20,24 @@ export function useSetReminders({ value }) {
             minutesCon.includes("PM") ? parseInt(hours) + 12 : hours
           ),
           minute: Number(minutes),
-          repeats: true,
+          repeats: false,
         };
 
         await Notifications.scheduleNotificationAsync({
           content: {
             title: "Báo nhắc",
-            body: text,
+            body: "text",
           },
-          trigger: schedulingOptions,
+          // trigger: schedulingOptions,
+          trigger: {
+            seconds: 10, // Hẹn giờ sau 10 giây
+          },
         });
-        console.log("schedulingOptions :", schedulingOptions);
+        console.log("Scheduled notification with options:", schedulingOptions);
       });
     });
   };
+
   useEffect(() => {
     const subscription = Notifications.addNotificationReceivedListener(
       (notification) => {
@@ -39,5 +47,26 @@ export function useSetReminders({ value }) {
 
     return () => subscription.remove();
   }, [value]);
+
+  // Gọi hàm scheduleNoti để lên lịch thông báo
   scheduleNoti();
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+
+  const { status: existingStatus } = await Notifications.getPermissionsAsync();
+  let finalStatus = existingStatus;
+  if (existingStatus !== "granted") {
+    const { status } = await Notifications.requestPermissionsAsync();
+    finalStatus = status;
+  }
+  if (finalStatus !== "granted") {
+    alert("Failed to get push token for push notification!");
+    return;
+  }
+
+  // Lấy Expo Push Token
+  token = (await Notifications.getExpoPushTokenAsync()).data;
+  return token;
 }
